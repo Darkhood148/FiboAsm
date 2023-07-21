b1f813c4fb437c2a5c63eeb49e1200d63d5a596a
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2023-07-22 02:56:37 +0530

    Takes double digit number input now upto 48; Add minor optimizations

--- fibo.asm
+++ fibo.asm
@@ -5,21 +5,21 @@ STDOUT    equ 1
 SYS_READ  equ 3
 
 section .data
-   inpline db 'Enter number of Fibonacci terms you want! (n>=1)', 0xa
+   inpline db 'Enter number of Fibonacci terms you want! (48=>n>=1)', 0xa
    inplen equ $ - inpline
    nl db 0xa
    nlen equ $ - nl
    radix dd 10
 
 section .bss
+   ns resb 2
    n resb 1
    num1 resd 1
    num2 resd 1
    num3 resd 1
    temp resd 1
-   arr resb 10 ;array for storing individual digits in the number
+   arr resb 13 ;array for storing individual digits in the number. Idk y it needs 13 to store at max 10 digits. Wrong values if u try 10 instead of 13
    lv resb 1 ;looping variable
-   temp2 resb 1
 
 section .text
    global _start
@@ -41,8 +41,8 @@ print_nl: ;Function for printing new line
    ret
 
 _start:
-   mov dword [num1], 0
-   mov dword [num2], 1
+   mov dword [num1], '0'
+   mov dword [num2], '1'
 
    mov edx, inplen
    mov ecx, inpline
@@ -52,47 +52,64 @@ _start:
 
    mov eax, SYS_READ
    mov ebx, STDIN
-   mov ecx, n
-   mov edx, 1
+   mov ecx, ns
+   mov edx, 2
    int 0x80
 
-   sub dword [n],'0'
+   cmp byte [ns+1], 0xa ;second char is newline character indicating only one digit number
+   je sd ;single digit
 
-   mov eax, dword [num1]
-   mov dword [temp], eax
-   add dword [temp], '0'
+   mov edi, ns
+   xor eax, eax
+   mov al, byte [ns]
+   sub eax, '0'
+   mov ebx, 10
+   mul bx
+   mov byte [n], al ;getting first digit
+
+   sub byte [ns+1], '0'
+   mov al, byte [ns+1]
+   add byte [n], al ;getting second digit
+   jmp atl1
+
+sd:
+
+   sub byte [ns], '0'
+   mov al, byte [ns]
+   mov byte [n], al
+
+atl1: ;at least 1
 
    mov edx, 4
-   mov ecx, temp
+   mov ecx, num1
    mov ebx, STDOUT
    mov eax, SYS_WRITE
    int 0x80
 
    call print_nl
 
-   cmp dword [n], 1
+   cmp byte [n], 1
    jg atl2
    jmp _exit
 
 atl2: ;at least 2
-   mov eax, dword [num2]
-   mov dword [temp], eax
-   add dword [temp], '0'
 
    mov edx, 4
-   mov ecx, temp
+   mov ecx, num2
    mov ebx, STDOUT
    mov eax, SYS_WRITE
    int 0x80
 
    call print_nl
 
-   cmp dword [n], 2
+   cmp byte [n], 2
    jg atl3
    jmp _exit
 
 atl3: ;at least 3 
-   sub dword [n],2
+   sub dword [n],2 ;2 have already been printed
+   sub dword [num1], '0' ;ASCII to number
+   sub dword [num2], '0' ;ASCII to number
    mov ecx, [n]
 
    l1:
@@ -104,38 +121,32 @@ atl3: ;at least 3
    mov eax, dword [num1]
    add dword [num3], eax
 
-   ;call print_nl
-
    mov byte [lv], 0
    mov ecx, dword [num3]
 
-   ;call print_nl
    mov edi, arr
    
    l2: ;loop for storing digits in array form
    mov eax, ecx
    mov edx,0
-   ;call print_nl
    mov ebx, dword [radix] ;radix = 10
    div ebx ;num3/10
 
    mov ecx, eax ;storing quotient in ecx for cheking in loop condition at the end
    mov [edi], edx ;storing remainder of division in array
-   add byte [lv], 1
+   inc byte [lv]
    inc edi
    cmp ecx, 0
    jnz l2
 
-   mov ecx, dword [lv] ;No. of times loop l3 should execute
+   mov ecx, [lv] ;No. of times loop l3 should execute
    dec edi
 
    l3: ;for printing individual digits
    push ecx
-   mov edx, 4
-   mov ebx, [edi]
-   mov dword [temp], ebx
-   add dword [temp], '0'
-   mov ecx, temp
+   mov edx, 1
+   mov ecx, edi
+   add byte [ecx], '0'
    mov ebx, STDOUT
    mov eax, SYS_WRITE
    int 0x80
